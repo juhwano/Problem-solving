21194c29a174f8b200a80d9408a748fa0b59522b
JUHWAN OH <calculus9006@gmail.com>
2024-03-21 22:37:24 +0900

    [Silver III] Title: 소수 구하기, Time: 308 ms, Memory: 38792 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -3,20 +3,20 @@
 # 소수를 구하는 방법
 # 1. 2부터 n-1까지 모두 나눠서 나눠지는 수가 없으면 소수이다. (시간 초과)
 # 2. 2부터 n의 제곱근까지만 나눠서 나눠지는 수가 없으면 소수이다. (에라토스테네스의 체)
-import sys
-M, N = map(int, sys.stdin.readline().split())  # 사용자로부터 두 개의 숫자를 입력받아 M과 N에 저장
+n, m = map(int, input().split())  # 사용자로부터 두 개의 숫자를 입력받아 n과 m에 저장
 
+prime_list = [True]*(m+1)  # m+1 개의 True로 이루어진 리스트 생성
+prime_list[0] = False  # 0은 소수가 아니므로 False로 변경
+prime_list[1] = False  # 1은 소수가 아니므로 False로 변경
 
-def eratosthenes(num):  # 소수 판별 함수
-    if num == 1:  # 1은 소수가 아니므로 False 반환
-        return False
-    else:  # num을 2부터 num의 제곱근까지의 모든 숫자로 나눈다. 어떤 숫자로 나눠떨어진다면 num은 소수가 아니므로 False 반환
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True  # num이 소수인 경우
+# 에라토스테네스의 체 알고리즘
+# 2부터 N의 제곱근까지 반복(소수라면 그 숫자의 배수를 모두 소수가 아니라고 표시)
+for i in range(2, int(m**0.5)+1):
+    if prime_list[i]:
+        for j in range(i*2, m+1, i):
+            prime_list[j] = False
 
-
-for i in range(M, N + 1):
-    if eratosthenes(i):
-        print(i)  # 소수인 경우 출력
+# M부터 N까지의 모든 숫자에 대해, 범위 내의 소수만 출력
+for i in range(n, m+1):
+    if prime_list[i]:
+        print(i)
