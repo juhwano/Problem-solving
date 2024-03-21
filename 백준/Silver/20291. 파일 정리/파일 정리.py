7b6cebe22302dd363f30ad4815d10b0908771312
JUHWAN OH <calculus9006@gmail.com>
2024-03-21 23:40:12 +0900

    [Silver III] Title: 파일 정리, Time: 2084 ms, Memory: 43604 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.py
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -1,11 +1,8 @@
+file = {}
 N = int(input())
-
-extension = dict()
 for _ in range(N):
-    word = input().split('.')
-    if word[1] in extension:
-        extension[word[1]] += 1
-    else:
-        extension[word[1]] = 1
+    extension = input().split('.')[1]
+    file[extension] = file.get(extension, 0) + 1
 
-[print(key, value) for (key, value) in sorted(extension.items())]
+for (key, value) in sorted(file.items()):
+    print("{} {}".format(key, value))
