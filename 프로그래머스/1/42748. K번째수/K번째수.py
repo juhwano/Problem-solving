dc2d6e820aa69415f7b32296634e363d3ffb8739
JUHWAN OH <calculus9006@gmail.com>
2024-04-10 23:53:58 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,16 +1,6 @@
 def solution(array, commands):
     answer = []
     for command in commands:
-        if command[0] == command[1]:
-            answer.append(array[command[0]-1])
-            continue
-        start = command[0]-1
-        end = command[1]
-        newArr= array[start:end]
-        newArr.sort()
-
-        if len(newArr) >= command[2]:
-            answer.append(newArr[command[2]-1])
-        else:
-            raise
+        i,j,k = command
+        answer.append(sorted(array[i-1:j])[k-1])
     return answer
