28389aaedaa4dd1bf3dbff46172518d501e2a7a8
JUHWAN OH <calculus9006@gmail.com>
2024-04-12 00:49:38 +0900

    Update 콜라 문제.py

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,7 +1,8 @@
-import sys
-
-sys.setrecursionlimit(3000)
-
 def solution(a, b, n):
-    if n < a: return 0
-    return b + solution(a, b, n-a+b) if n >= a else 0
+    answer = 0
+    while n >= a:
+        quotient, remainder = divmod(n, a)  # n을 a로 나누어, 몫과 나머지를 구함
+        answer += quotient * b  # 새로 얻은 콜라 병의 수를 총량에 더함
+        n = quotient * b + remainder  # 남은 병의 수를 업데이트함. 새 병과 남은 빈 병을 합침
+
+    return answer  
