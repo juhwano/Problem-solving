363d3ebbc047b96280ef9afc20efa149a3ac0854
JUHWAN OH <calculus9006@gmail.com>
2024-04-12 00:02:24 +0900

    [level 1] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,8 +1,7 @@
-def solution(a, b, n):
-    answer = 0
-    while n >= a:
-        quotient, remainder = divmod(n, a)  # n을 a로 나누어, 몫과 나머지를 구함
-        answer += quotient * b  # 새로 얻은 콜라 병의 수를 총량에 더함
-        n = quotient * b + remainder  # 남은 병의 수를 업데이트함. 새 병과 남은 빈 병을 합침
+import sys
 
-    return answer  
+sys.setrecursionlimit(3000)
+
+def solution(a, b, n):
+    if n < a: return 0
+    return b + solution(a, b, n-a+b) if n >= a else 0
