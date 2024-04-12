bf33e96095a062e9ed6f0a7b315f4e123ac55ea3
JUHWAN OH <calculus9006@gmail.com>
2024-04-12 17:50:29 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,11 +1,9 @@
-def solution(a, b):
-    days = ['FRI', 'SAT', 'SUN','MON', 'TUE', 'WED', 'THU']
-    months = [31,29,31,30,31,30,31,31,30,31,30,31]
-    sum_days = 0
-    for i in range(a-1):
-        sum_days += months[i]
-    sum_days += b
-    target_day = sum_days % 7 - 1
-    answer = days[target_day]
+DAYS = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED']
+MONTHS = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
+def solution(a, b):
+    # sum 함수를 사용하여 sum_days를 계산합니다.
+    sum_days = sum(MONTHS[:a-1]) + b
+    # sum_days를 7로 나눈 나머지를 사용하여 요일을 찾습니다.
+    answer = DAYS[sum_days % 7]
     return answer
