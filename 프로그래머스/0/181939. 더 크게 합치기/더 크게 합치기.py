20c6f9bd25851f7006ececb3c35b1d85f6c042ff
JUHWAN OH <calculus9006@gmail.com>
2024-08-04 14:42:54 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,9 +1,2 @@
 def solution(a, b):
-    str_a=str(a)
-    str_b=str(b)
-    str_ab=str_a+str_b
-    str_ba=str_b+str_a
-    int_ab= int(str_ab)
-    int_ba= int(str_ba)
-    answer = int_ab if int_ab > int_ba else int_ba
-    return answer
+    return max(int(f"{a}{b}"), int(f"{b}{a}"))
