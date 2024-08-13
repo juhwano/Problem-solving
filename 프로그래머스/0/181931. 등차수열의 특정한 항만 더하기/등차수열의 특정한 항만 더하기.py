1c44a0348f3076f913dea773370feb3ef0c9dca2
JUHWAN OH <calculus9006@gmail.com>
2024-08-13 23:10:42 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -1,10 +1,7 @@
 def solution(a, d, included):
     # 등차수열의 일반항
     # An = A1 + B(n-1)
-    sum = 0
-    # 항수를 알아야한다.
-    for i, bool in enumerate(included):
-        if bool == True:
-            sum += a + i * d
-
-    return sum
+    answer = 0
+    for i in range(len(included)):
+        answer += (a + d * i) * int(included[i])
+    return answer
