d37fbc8f56ea878d5d75f6bfc62a038b5db4c0ef
JUHWAN OH <calculus9006@gmail.com>
2024-09-01 20:20:33 +0900

    [Bronze V] Title: 개수 세기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,12 +1,5 @@
 n = int(input())
-li = map(int, input().split())
+li = list(map(int, input().split()))
 target = int(input())
 
-cnt=0
-
-for j in li:
-    if j == target:
-        cnt+=1
-        
-print(cnt)
-    
+print(li.count(target))
