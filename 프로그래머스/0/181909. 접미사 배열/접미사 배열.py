d51ff152a886641772eeb09af669e5d654318dd1
JUHWAN OH <calculus9006@gmail.com>
2024-09-02 02:27:48 +0900

    [level 0] Title: 접미사 배열, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
@@ -1,8 +1,2 @@
 def solution(my_string):
-    list_size= len(my_string)
-    answer = []
-    for i in range(1,list_size+1,1):
-        answer.append(my_string[-i:])
-    answer.sort()
-
-    return answer
+    return sorted(my_string[i:] for i in range(len(my_string)))
