c365fc43ec5c28205a876dbbd6c5abaf9bcf15d1
JUHWAN OH <calculus9006@gmail.com>
2024-10-29 18:30:34 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -1,35 +1,17 @@
-from collections import Counter
 def solution(a, b, c, d):
-    count_arr=Counter([a,b,c,d])
-    score=0
-    max_counter = count_arr.most_common(1)[0][1]
-    reverse_dict = {value:key for key,value in count_arr.items()}
-    
-    if max_counter == 4:
-        score += 1111*a
-    elif max_counter == 3:
-        one_num = reverse_dict.get(1)
-        three_num = reverse_dict.get(3)
-        score += (10 * three_num + one_num)**2
-    elif max_counter == 2:
-        value_one = count_arr.most_common(2)[0][1]
-        value_two =count_arr.most_common(2)[1][1]
-        
-        if value_one == value_two:
-            key_one =count_arr.most_common(2)[0][0]
-            key_two =count_arr.most_common(2)[1][0]
-            score += (key_one + key_two) * abs(key_one - key_two)
+    nums = [a, b, c, d]
+    counts = [nums.count(i) for i in nums]
+    if max(counts) == 4:
+        return a * 1111
+    elif max(counts) == 3:
+        p = nums[counts.index(3)]
+        q = nums[counts.index(1)]
+        return (10 * p + q) ** 2
+    elif max(counts) == 2:
+        if min(counts) == 2:
+            return (a + c) * abs(a - c) if a == b else (a + b) * abs(a - b)
         else:
-            key_one =count_arr.most_common(3)[0][0]
-            key_two =count_arr.most_common(3)[1][0]
-            key_three =count_arr.most_common(3)[2][0]
-        
-            score += (key_two * key_three)
+            p = nums[counts.index(2)]
+            return (a * b * c * d) / p**2
     else:
-        arr =count_arr.most_common()
-        sort_arr = sorted(arr)
-        min_num = sort_arr[0][0]
-        
-        score += min_num
-
-    return score
+        return min(nums)
